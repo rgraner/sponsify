fbff26755a068436544c989cac0add4acfdae336
rgraner <ricardograner@gmail.com>
2023-07-20 11:57:53 +0200

    Add sponsorship plans section

--- views/src/components/sponsoredPage/SponsoredPage.js
+++ views/src/components/sponsoredPage/SponsoredPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SponsoredPage.css';
 
 function SponsoredPage() {
   const sponsors = [
@@ -7,6 +8,33 @@ function SponsoredPage() {
     'Company C',
     'Company D',
     'Company E',
+  ];
+
+  const sponsorshipPlans = [
+    {
+      name: 'Gold Plan',
+      price: '99',
+      benefits: [
+        'Logo placement on website',
+        'Social media promotion',
+        'VIP event access',
+      ],
+    },
+    {
+      name: 'Silver Plan',
+      price: '49',
+      benefits: [
+        'Logo placement on website',
+        'Social media promotion',
+      ],
+    },
+    {
+      name: 'Bronze Plan',
+      price: '19',
+      benefits: [
+        'Logo placement on website',
+      ],
+    },
   ];
 
   return (
@@ -24,15 +52,35 @@ function SponsoredPage() {
 
       <section>
         <h2>Sponsors</h2>
-        <ul className="no-list">
+        <ul className="items no-list">
           {sponsors.map((company, index) => (
             <li key={index}>{company}</li>
           ))}
       </ul>
       </section>
-      
 
-      
+      <section>
+        <h2>Plans</h2>
+        <div className="card">
+          {sponsorshipPlans.map((plan, index) => (
+            <div className="plan-card" key={index}>
+              <div className="plan-card-header">
+                <h3>{plan.name}</h3>
+                <p>{plan.price === 'Free' ? 'Free' : `€${plan.price}`}</p>
+              </div>
+              <div className="plan-card-body">
+                <ul>
+                  {plan.benefits.map((benefit, index) => (
+                    <li key={index}>{benefit}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="plan-card-footer">Buy Now</div>
+            </div>
+          ))}
+        </div>
+        
+      </section>
     </div>
   );
 }
