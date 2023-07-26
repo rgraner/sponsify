f4a89bffbab8f8b46ba1cf1535df8690ba910d24
rgraner <ricardograner@gmail.com>
2023-07-26 17:33:54 +0200

    Add .env

--- server/models/pool.js
+++ server/models/pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'your-db-username',
-  host: 'your-db-host',
-  database: 'your-db-name',
-  password: 'your-db-password',
-  port: 5432, // Default PostgreSQL port
-});
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
 
 module.exports = pool;
