06efe7d2fd549fc72d743040b369dcab4791a312
rgraner <ricardograner@gmail.com>
2023-08-23 13:07:06 +0100

    Create new entry to sponsor_projects table when checkout

--- server/controllers/checkout.js
+++ server/controllers/checkout.js
@@ -13,16 +13,33 @@ const checkout = async (req, res) => {
 
         // Check if a cart item exists
         if (cartItem.rows.length === 0) {
-            return res.status(404).json({ message: 'No items in the cart' });
-        }
+            return res.status(404).json({ message: 'No item in the cart' });
+        };
         
-        // Create a new order
+        // Create a new order using the plan_id from the cart item
         const { plan_id } = cartItem.rows[0];
         await pool.query(
             'INSERT INTO orders (sponsor_id, plan_id) VALUES ($1, $2) RETURNING *',
             [sponsorId, plan_id]
         );
-        
+
+        // Get the project_id associated with the plan
+        const project = await pool.query(
+            'SELECT project_id FROM plans WHERE id = $1',
+            [plan_id]
+        );
+
+        // Check if a project exists
+        if (project.rows.length === 0) {
+            return res.status(404).json({ message: 'Associated project with the plan not found' });
+        }
+
+        // Insert a new entry into sponsor_projects
+        const { project_id } = project.rows[0];
+        await pool.query(
+            'INSERT INTO sponsor_projects (project_id, sponsor_id) VALUES ($1, $2)',
+            [project_id, sponsorId]
+        );
         
         // Clear the sponsors's cart
         await pool.query('DELETE FROM cart WHERE sponsor_id = $1', [sponsorId]);
