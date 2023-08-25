f955f0cb828dc55ead3baae3c6f71a5727b72458
rgraner <ricardograner@gmail.com>
2023-08-25 20:47:28 +0100

    Remove project reducer

--- views/src/redux/reducers/index.js
+++ views/src/redux/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import projectsReducer from './projectsReducer'; 
-import projectReducer from './projectReducer'; 
 import projectsBySponsorReducer from './projectsBySponsorReducer';
 import sponsorsReducer from './sponsorsReducer';
 import sponsorsByProjectReducer from './sponsorsByProjectReducer';
@@ -8,7 +7,6 @@ import plansReducer from './plansReducer';
 
 const rootReducer = combineReducers({
   projects: projectsReducer,
-  project: projectReducer,
   projectsBySponsor: projectsBySponsorReducer,
   sponsors: sponsorsReducer,
   sponsorsByProject: sponsorsByProjectReducer,
