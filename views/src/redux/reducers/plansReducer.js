0b4689911e43a88a32abcf3f4d1e36148db19ce4
rgraner <ricardograner@gmail.com>
2023-08-28 00:14:04 +0100

    Clean up 'plansReducer' file

--- views/src/redux/reducers/plansReducer.js
+++ views/src/redux/reducers/plansReducer.js
@@ -1,30 +1,3 @@
-// const initialState = [
-//     {
-//       name: 'Gold Plan',
-//       price: '99',
-//       benefits: [
-//         'Logo placement on website',
-//         'Social media promotion',
-//         'VIP event access',
-//       ],
-//     },
-//     {
-//       name: 'Silver Plan',
-//       price: '49',
-//       benefits: [
-//         'Logo placement on website',
-//         'Social media promotion',
-//       ],
-//     },
-//     {
-//       name: 'Bronze Plan',
-//       price: '19',
-//       benefits: [
-//         'Logo placement on website',
-//       ],
-//     },
-// ];
-
 // Action Type:
 const FETCH_PLANS_SUCCESS = 'FETCH_PLANS_SUCCESS';
 
